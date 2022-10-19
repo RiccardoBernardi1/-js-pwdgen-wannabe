83a9e944497dd091b4163f2c8346f2cabeb071fa
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-19 14:10:57 +0200

    Aggiunge prompt per cognome  e colore e dichiara variabile password

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 // 1. chiedere il nome all' utente e salvarlo in una variabile "nome"
-const nome = prompt('Inserisci il tuo nome')
-// 2. chiedi il cognome all' utente e salvarlo in una variabile "cognome"
-// 3. chiedi il colore preferito all' utente e salvarlo in una variabile "colorePreferito"
-// 4. genera una variabile "password" contenente la concatenazione delle variabili precedenti e 76 alla fine
+const nome = prompt('Inserisci il tuo nome');
+// 2. chiedere il cognome all' utente e salvarlo in una variabile "cognome"
+const cognome = prompt('Inserisci il tuo cognome');
+// 3. chiedere il colore preferito all' utente e salvarlo in una variabile "colorePreferito"
+const colorePreferito = prompt('Inserisci il tuo colore preferito');
+// 4. generare una variabile "password" contenente la concatenazione delle variabili precedenti con 76 alla fine
+const password = (nome+cognome+colorePreferito+76);
+// 5. mostra a schermo la variabile pasword
