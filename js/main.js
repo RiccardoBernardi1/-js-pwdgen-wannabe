8e03966c3f7f52b87c3ba53ddfeba816d87dd49e
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-19 14:03:31 +0200

    Aggiunge titolo pagina e prompt per il nome

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// 1. chiedere il nome all' utente e salvarlo in una variabile "nome"
+const nome = prompt('Inserisci il tuo nome')
+// 2. chiedi il cognome all' utente e salvarlo in una variabile "cognome"
+// 3. chiedi il colore preferito all' utente e salvarlo in una variabile "colorePreferito"
+// 4. genera una variabile "password" contenente la concatenazione delle variabili precedenti e 76 alla fine
